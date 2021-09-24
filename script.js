1f81c7f599ae6f1a5b727f3d3aae949f47e37ab9
Guilherme <gui.malachias@gmail.com>
2021-09-24 13:21:52 -0300

    About me responsivo

--- script.js
+++ script.js
@@ -16,32 +16,3 @@ function scrollToAbout(event) {
         behavior: "smooth"
     });
 }
-
-let time = 3000,
-    currentImageIndex = 0,
-    images = document
-        .querySelectorAll("#sliderWorkshop img")
-max = images.length;
-
-function nextImage() {
-
-    images[currentImageIndex]
-        .classList.remove("selected")
-
-    currentImageIndex++
-
-    if (currentImageIndex >= max)
-        currentImageIndex = 0
-
-    images[currentImageIndex]
-        .classList.add("selected")
-}
-
-function start() {
-    setInterval(() => {
-        // troca de imagem
-        nextImage()
-    }, time)
-}
-
-window.addEventListener("load", start);
